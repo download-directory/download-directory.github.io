68bdb071db72c282f8bbb8317872244bbc856e3a
Florian Reuschel <Florian.Reuschel@gmx.de>
2019-11-01 10:12:51 +0100

    Add support for downloading folders from private repos (#26)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import saveFile from 'save-file';
 import listContent from 'list-github-dir-content';
 
 // Matches '/<re/po>/tree/<ref>/<dir>'
-const repoDirRegex = /^[/](.+[/].+)[/]tree[/]([^/]+)[/](.*)/;
+const urlParserRegex = /^[/]([^/]+)[/]([^/]+)[/]tree[/]([^/]+)[/](.*)/;
 
 function updateStatus(status, ...extra) {
 	const el = document.querySelector('.status');
@@ -42,7 +42,7 @@ async function waitForToken() {
 	}
 }
 
-async function validateInput(repo) {
+async function fetchRepoInfo(repo) {
 	const response = await fetch(`https://api.github.com/repos/${repo}`, {
 		headers: {
 			Authorization: `Bearer ${localStorage.token}`
@@ -75,27 +75,23 @@ async function validateInput(repo) {
 		throw new Error('Fetch error');
 	}
 
-	const repoMetadata = await response.json();
-
-	if (repoMetadata.private) {
-		updateStatus('⚠ Private repositories are <a href="https://github.com/download-directory/download-directory.github.io/issues/7">not supported yet</a>.');
-		throw new Error('Private repository');
-	}
+	return response.json();
 }
 
 async function init() {
 	await waitForToken();
 
-	let repo;
+	let user;
+	let repository;
 	let ref;
 	let dir;
 
 	try {
 		const query = new URLSearchParams(location.search);
 		const parsedUrl = new URL(query.get('url'));
-		[, repo, ref, dir] = repoDirRegex.exec(parsedUrl.pathname);
+		[, user, repository, ref, dir] = urlParserRegex.exec(parsedUrl.pathname);
 
-		console.log('Source:', {repo, ref, dir});
+		console.log('Source:', {user, repository, ref, dir});
 	} catch {
 		return updateStatus();
 	}
@@ -107,34 +103,67 @@ async function init() {
 
 	updateStatus('Retrieving directory info…');
 
-	await validateInput(repo);
+	const {private: repoIsPrivate} = await fetchRepoInfo(`${user}/${repository}`);
 
-	const files = await listContent.viaTreesApi(`${repo}#${ref}`, decodeURIComponent(dir), localStorage.token);
+	const files = await listContent.viaTreesApi({
+		user,
+		repository,
+		ref,
+		directory: decodeURIComponent(dir),
+		token: localStorage.token,
+		getFullData: true
+	});
+
 	if (files.length === 0) {
 		updateStatus('No files to download');
 		return;
 	}
 
-	updateStatus(`Downloading (0/${files.length}) files…`, '\n• ' + files.join('\n• '));
+	updateStatus(`Downloading (0/${files.length}) files…`, '\n• ' + files.map(file => file.path).join('\n• '));
 
-	let downloaded = 0;
-	const zip = new JSZip();
 	const controller = new AbortController();
-	const download = async path => {
-		const response = await fetch(`https://raw.githubusercontent.com/${repo}/${ref}/${path}`, {
+
+	const fetchPublicFile = async file => {
+		const response = await fetch(`https://raw.githubusercontent.com/${user}/${repository}/${ref}/${file.path}`, {
 			signal: controller.signal
 		});
 
 		if (!response.ok) {
-			throw new Error(`HTTP ${response.statusText} for ${path}`);
+			throw new Error(`HTTP ${response.statusText} for ${file.path}`);
 		}
 
-		const blob = await response.blob();
+		return response.blob();
+	};
+
+	const fetchPrivateFile = async file => {
+		const response = await fetch(file.url, {
+			headers: {
+				Authorization: `Bearer ${localStorage.token}`
+			},
+			signal: controller.signal
+		});
+
+		if (!response.ok) {
+			throw new Error(`HTTP ${response.statusText} for ${file.path}`);
+		}
+
+		const {content} = await response.json();
+		const decoder = await fetch(`data:application/octet-stream;base64,${content}`);
+		return decoder.blob();
+	};
+
+	let downloaded = 0;
+	const zip = new JSZip();
+
+	const download = async file => {
+		const blob = repoIsPrivate ?
+			await fetchPrivateFile(file) :
+			await fetchPublicFile(file);
 
 		downloaded++;
-		updateStatus(`Downloading (${downloaded}/${files.length}) files…`, path);
+		updateStatus(`Downloading (${downloaded}/${files.length}) files…`, file.path);
 
-		zip.file(path.replace(dir + '/', ''), blob, {
+		zip.file(file.path.replace(dir + '/', ''), blob, {
 			binary: true
 		});
 	};
@@ -161,7 +190,7 @@ async function init() {
 		type: 'blob'
 	});
 
-	await saveFile(zipBlob, `${repo} ${ref} ${dir}.zip`.replace(/\//, '-'));
+	await saveFile(zipBlob, `${user} ${repository} ${ref} ${dir}.zip`.replace(/\//, '-'));
 	updateStatus(`Downloaded ${downloaded} files! Done!`);
 }
 
