6804ca04b86ec68772e98a2bd4fdc4a803554500
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-01-28 22:45:06 +0900

    Add support for slashes in branch names (#107)

--- index.js
+++ index.js
@@ -14,6 +14,32 @@ async function maybeResponseLfs(response) {
 	}
 }
 
+async function repoListingSlashblanchSupport(ref, dir, repoListingConfig) {
+	let files;
+	const dirParts = decodeURIComponent(dir).split('/');
+	while (dirParts.length >= 0) {
+		try {
+			files = await listContent.viaTreesApi(repoListingConfig); // eslint-disable-line no-await-in-loop
+			break;
+		} catch (error) {
+			if (error.message === 'Not Found') {
+				ref += '/' + dirParts.shift();
+				repoListingConfig.directory = dirParts.join('/');
+				repoListingConfig.ref = ref;
+			} else {
+				throw error;
+			}
+		}
+	}
+
+	if (files.length === 0 && files.truncated) {
+		updateStatus('Warning: It’s a large repo and this it take a long while just to download the list of files. You might want to use "git sparse checkout" instead.');
+		files = await listContent.viaContentsApi(repoListingConfig);
+	}
+
+	return [files, ref];
+}
+
 function updateStatus(status, ...extra) {
 	const element = document.querySelector('.status');
 	if (status) {
@@ -141,11 +167,8 @@ async function init() {
 		token: localStorage.token,
 		getFullData: true,
 	};
-	let files = await listContent.viaTreesApi(repoListingConfig);
-	if (files.length === 0 && files.truncated) {
-		updateStatus('Warning: It’s a large repo and this it take a long while just to download the list of files. You might want to use "git sparse checkout" instead.');
-		files = await listContent.viaContentsApi(repoListingConfig);
-	}
+	let files;
+	[files, ref] = await repoListingSlashblanchSupport(ref, dir, repoListingConfig);
 
 	if (files.length === 0) {
 		updateStatus('No files to download');
